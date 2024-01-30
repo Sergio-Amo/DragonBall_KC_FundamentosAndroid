2807c803f2550043f95be3e19da5eb5bd4d15538
Sergio Amo <sergio.amo@outlook.com>
2024-01-31 00:44:13 +0100

    Open list fragment by onCreate activity home

--- app/src/main/java/com/kc/dragonball_kc_fundamentos/ui/home/HomeActivity.kt
+++ app/src/main/java/com/kc/dragonball_kc_fundamentos/ui/home/HomeActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import com.kc.dragonball_kc_fundamentos.databinding.ActivityHomeBinding
+import com.kc.dragonball_kc_fundamentos.ui.home.list.HeroesFragment
 import com.kc.dragonball_kc_fundamentos.utils.INFO_HOME_ACTIVITY
 
 interface HomeInterface {
@@ -22,16 +23,23 @@ class HomeActivity : AppCompatActivity(), HomeInterface {
         }
     }
 
-    private lateinit var token: String
     private lateinit var binding: ActivityHomeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        (intent.getStringExtra(INFO_HOME_ACTIVITY) ?: "").apply {
+            showHeroList(this)
+        }
+    }
 
-        token = intent.getStringExtra(INFO_HOME_ACTIVITY) ?: ""
-        binding.fragmentContainer
+    private fun showHeroList(token: String) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(binding.fragmentContainer.id, HeroesFragment(token, this))
+            //.addToBackStack(null) // Put fragment into back stack
+            .commit()
     }
 
     override fun showLoading(show: Boolean) {
