a8099881bc87ea8e55de86d1922adddeed9f93dc
Sergio Amo <sergio.amo@outlook.com>
2024-01-31 17:54:48 +0100

    Present all hero information to the layout and prevent events from passing thru the detail fragment

--- app/src/main/java/com/kc/dragonball_kc_fundamentos/ui/home/HeroDetailsFragment.kt
+++ app/src/main/java/com/kc/dragonball_kc_fundamentos/ui/home/HeroDetailsFragment.kt
@@ -1,11 +1,15 @@
 package com.kc.dragonball_kc_fundamentos.ui.home
 
+import android.content.res.ColorStateList
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
+import com.bumptech.glide.Glide
+import com.kc.dragonball_kc_fundamentos.R
 import com.kc.dragonball_kc_fundamentos.databinding.FragmentHeroDetailsBinding
 import com.kc.dragonball_kc_fundamentos.model.Hero
 
@@ -31,7 +35,23 @@ class HeroDetailsFragment(private val hero: Hero, private val homeInterface: Hom
     }
 
     private fun showHero() {
+        //name
         binding.heroName.text = hero.name
+        //health
+        binding.healthBar.progress = hero.health
+        binding.healthBar.max = hero.maxHealth
+        binding.healthBar.progressTintList = when {
+            hero.health < 26 -> ColorStateList.valueOf(Color.RED)
+            hero.health < 51 -> ColorStateList.valueOf(Color.YELLOW)
+            else -> ColorStateList.valueOf(Color.GREEN)
+        }
+        // Image
+        Glide
+            .with(binding.root)
+            .load(hero.photo)
+            .centerCrop()
+            .placeholder(R.drawable.ic_launcher_foreground)
+            .into(binding.heroImage)
     }
 
 }
